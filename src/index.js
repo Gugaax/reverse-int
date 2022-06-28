ff60395c65789e6994828b808b8f4f2e67c7c59d
Gugaax <Gugaax@gmail.com>
2022-06-28 21:22:42 +0300

    created revers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    // преобразовали к абсолютному значению
+
+    let new_n = n.toString()  
+    // преоразовали в строку
+    .split("")
+    // преобразовали  в массив
+    .reverse()
+    // перевернули
+    .join("");
+    // склеили в строку
+       
+    return (new_n);
 }
